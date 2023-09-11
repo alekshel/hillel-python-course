094cf0c9b20c943def3460828e984287e7eac1b6
alekshel <alek_shel@icloud.com>
2023-09-11 16:00:06 +0600

    App students

--- lesson-05-django/django_base/django_base/urls.py
+++ lesson-05-django/django_base/django_base/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from students import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('generate-student/', views.generate_student, name='generate_student'),
+    path('generate-students/', views.generate_students, name='generate_students'),
 ]
